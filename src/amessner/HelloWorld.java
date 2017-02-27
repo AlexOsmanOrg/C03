7a09a0892ebc46417dd9eaadc00e19a037adc175
amessner <alexander.messner.am@gmail.com>
2017-02-27 10:12:55 +0100

    zweiten Consolen Output hinzugefügt

--- src/amessner/HelloWorld.java
+++ src/amessner/HelloWorld.java
@@ -3,6 +3,7 @@ package amessner;
 public class HelloWorld {
 	public static void GruﬂvonAlex(){
 		System.out.println("Hallo Osman");
+		System.out.println("Hallo ich bin Alex");
 	}
 	public static void main(String[] args) {
 		GruﬂvonAlex();
